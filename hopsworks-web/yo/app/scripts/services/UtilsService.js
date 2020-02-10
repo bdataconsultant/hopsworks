e7089e0d15192c19c8223488b36af16ec10cdcb8
f.lavorato <f.lavorato@almaviva.it>
2020-02-10 18:10:01 +0100

    dataset file download updated. Admin interface endpoint updated

--- hopsworks-web/yo/app/scripts/services/UtilsService.js
+++ hopsworks-web/yo/app/scripts/services/UtilsService.js
@@ -65,7 +65,24 @@ angular.module('hopsWorksApp')
             },
             setDatasetName: function (datasetname) {
               datasetName = datasetname;
-            }
+            },
+            download: function (data, filename, type) {
+              var file = new Blob([data], {type: type});
+              if (window.navigator.msSaveOrOpenBlob) // IE10+
+                  window.navigator.msSaveOrOpenBlob(file, filename);
+              else { // Others
+                  var a = document.createElement("a"),
+                          url = URL.createObjectURL(file);
+                  a.href = url;
+                  a.download = filename;
+                  document.body.appendChild(a);
+                  a.click();
+                  setTimeout(function() {
+                      document.body.removeChild(a);
+                      window.URL.revokeObjectURL(url);  
+                  }, 0); 
+              }
+            },
           };
         });
 
