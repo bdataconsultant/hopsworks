d70f4a5ce9bc82ad1d0c01280ee4a49467f755cd
Castrese Riccio <r.castrese@almaviva.it>
2020-03-17 11:15:10 +0100

    last try

---
+++ hopsworks-common/src/main/java/io/hops/hopsworks/common/dao/util/ErrorMessage.java
@@ -0,0 +1,14 @@
+package io.hops.hopsworks.common.dao.util;
+
+public class ErrorMessage {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
