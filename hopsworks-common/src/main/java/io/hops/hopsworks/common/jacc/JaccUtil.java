72d777ec5f0c4d9cf55858de2ad7f8b48fd7ba7d
a.cosenza <a.cosenza@almaviva.it>
2020-04-23 13:29:57 +0200

    ldap login - log principal class better

--- hopsworks-common/src/main/java/io/hops/hopsworks/common/jacc/JaccUtil.java
+++ hopsworks-common/src/main/java/io/hops/hopsworks/common/jacc/JaccUtil.java
@@ -38,7 +38,7 @@ public final class JaccUtil {
      *         <li>the method has not been called within a Servlet invocation context</li>
      *         </ul>
      */
-    public static Set<String> getCallerWebRoles(String servletName) {
+    public static Set<String> getCallerRoles(String servletName) {
         // get current subject
         Subject subject = getSubject();
         if (subject == null) {
@@ -47,48 +47,13 @@ public final class JaccUtil {
             return NO_ROLES;
         }
         Set<Principal> principals = subject.getPrincipals();
-        principals.forEach(p -> LOGGER.log(Level.INFO, "LOGGED_PRINCIPAL: " + p + ", class: " + p.getClass()));
+        principals.forEach(p -> LOGGER.log(Level.INFO, "LOGGED_PRINCIPAL: " + p + ", canonical: " + p.getClass().getCanonicalName() + ", name: " + p.getClass().getName()));
         if (principals.isEmpty()) {
             // unauthenticated?
             LOGGER.log(Level.INFO, "No roles!");
             return NO_ROLES;
         }
-        // construct a domain for querying the policy; the code source shouldn't matter, as far as
-        // JACC permissions are concerned
-        ProtectionDomain domain = new ProtectionDomain(new CodeSource(null, (Certificate[]) null), null, null,
-                principals.toArray(new Principal[principals.size()]));
-        // get all permissions accorded to those principals
-        PermissionCollection pc = Policy.getPolicy().getPermissions(domain);
-        // cause resolution of WebRoleRefPermissions, if any, in the collection, if still unresolved
-        pc.implies(DUMMY_WEB_ROLE_REF_PERM);
-        Enumeration<Permission> e = pc.elements();
-        if (!e.hasMoreElements()) {
-            // nothing granted, hence no roles
-            LOGGER.log(Level.INFO, "No permissions!");
-            return NO_ROLES;
-        }
         Set<String> roleNames = NO_ROLES;
-        // iterate over the collection and eliminate duplicates
-        while (e.hasMoreElements()) {
-            Permission p = e.nextElement();
-            LOGGER.log(Level.INFO, "permission: " + p);
-            // only interested in Servlet container security-role(-ref) permissions
-            if (p instanceof WebRoleRefPermission) {
-                String candidateRoleName = p.getActions();
-                LOGGER.log(Level.INFO, "candidateRoleName: " + candidateRoleName);
-                // - ignore the "any-authenticated-user" role (only collect it if your
-                // application has actually declared a role named "**")
-                // - also restrict to the scope of the Servlet identified by the servletName
-                // argument, unless null
-                if (!"**".equals(candidateRoleName) && ((servletName == null) || servletName.equals(p.getName()))
-                        && ((roleNames == NO_ROLES) || !roleNames.contains(candidateRoleName))) {
-                    if (roleNames == NO_ROLES) {
-                        roleNames = new HashSet<>();
-                    }
-                    roleNames.add(candidateRoleName);
-                }
-            }
-        }
         return roleNames;
     }
 
