7835748d1a757ba86ba0934fc041a58fff1c500e
Francesco Lavorato <f.lavorato@almaviva.it>
2020-06-12 12:32:40 +0200

    added Bari customer build profile

---
+++ hopsworks-web/yo/app/customer_assets/BARI/providers/customerConfig.js
@@ -0,0 +1,27 @@
+(function (angular) {
+  "use strict";
+  angular.module("hopsWorksApp").provider("customerConfig", function () {
+    var values = {
+      showPonFooter: true,
+      header: {
+        logoDir: "images/big-data-logo-header.png",
+        appContext: "bigdata",
+        appTitle: "Comune di Bari",
+        appUrlsConfig: {
+          bigdata: "/giotto-web",
+          iot: "/home",
+          admin: "/oneadmin",
+        },
+      },
+    };
+
+    return {
+      $get: function () {
+        return values;
+      },
+      set: function (constants) {
+        angular.extend(values, constants);
+      },
+    };
+  });
+})(angular);
