90406d20bc8ddaeaa7162919e86714d40825a391
Lavorato Francesco <F.Lavorato@almaviva.it>
2020-06-24 15:25:41 +0000

    Merged PR 11377: Reggio Calabria scp build profile added
    Reggio Calabria scp build profile added

---
+++ hopsworks-web/yo/app/customer_assets/REGGIO_SCP/providers/customerConfig.js
@@ -0,0 +1,28 @@
+(function (angular) {
+  "use strict";
+  angular.module("hopsWorksApp").provider("customerConfig", function () {
+    var values = {
+      showPonFooter: true,
+      header: {
+        logoDir: "images/big-data-logo-header.png", 
+        appContext: 'bigdata',
+        appTitle: 'CITTÀ DI REGGIO CALABRIA',
+        appUrlsConfig: {
+          bigdata: '/giotto-web/',
+          iot: '/home/',
+          admin: '/oneadmin/',
+          udm: "/udm-fe/"
+        }
+      }
+    };
+
+    return {
+      $get: function() {
+        return values;
+      },
+      set: function(constants) {
+        angular.extend(values, constants);
+      }
+    };
+  });
+})(angular);
